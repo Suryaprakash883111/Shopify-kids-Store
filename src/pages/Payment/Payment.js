3404e5f5e1843b6d46bcdded8648aa96c6b73493
Suryaprakash883111 <prakash883111@gmail.com>
2025-07-06 01:58:33 +0530

    Update Payment.js
    edit alert

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Card, Button, Form, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
